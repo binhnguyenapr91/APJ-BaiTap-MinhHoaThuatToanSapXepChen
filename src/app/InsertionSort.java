3c5d1a268885b2f26c901f2f65bc03ea44599d58
binhnguyenapr91 <binhnguyen.apr91@gmail.com>
2020-04-08 23:53:24 +0700

    init commit

--- src/app/InsertionSort.java
+++ src/app/InsertionSort.java
@@ -2,48 +2,27 @@ package app;
 
 public class InsertionSort {
 	public static double[] list = {4,3,2,7,6,5,1,8,9,0};
-	public static double[] outputList = {0,0,0,0,0,0,0,0,0,0};
-	public static int sizeOfOutput = 0;
-	
-	public static void insertionSort(double[] list) {
-		for(int i = 0; i<list.length;i++) {
-			outputList[i]=list[i];
-			selectionSort(outputList);
-			for(double item : outputList) {
-				System.out.print(item+" ");
-			}
-			System.out.println();
-			
-		}
-		
-	}
-	
-	public static void selectionSort(double[] list) {
-		for(int i = 0; i< list.length -1 ;i++) {
-			double currentMin = list[i];
-			int currentMinIndex = i;
-			
-			for(int j = i+1;j<list.length;j++) {
-				if (currentMin<list[j]) {
-					currentMin = list[j];
-					currentMinIndex = j;
-				}
-			}
-			
-			if(currentMinIndex!=i) {
-				list[currentMinIndex] = list[i];
-				list[i] = currentMin;
-			}
-			
-		}
+
+	public static double[] insertionSort(double[] arr){
+		int n = arr.length; 
+        for (int i = 1; i < n; ++i) { 
+            double key = arr[i]; 
+            int j = i - 1; 
+
+            while (j >= 0 && arr[j] < key) { 
+                arr[j + 1] = arr[j]; 
+                j = j - 1; 
+            } 
+            arr[j + 1] = key; 
+        }
+        return arr;
 	}
 	
 	public static void main(String args[]) {
 		
-		insertionSort(list);
-		System.out.println(outputList);
 		
-		for(double item:outputList) {
+		insertionSort(list);
+		for(double item:list) {
 			System.out.print(item + " ");
 		}
 	}
